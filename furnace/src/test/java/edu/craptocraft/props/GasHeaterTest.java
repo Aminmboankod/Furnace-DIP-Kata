7732d0db6d6fe298b5842e6d208eee99732b0089
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-04-12 17:39:52 +0200

    refactor(GasHeaterTest)

--- furnace/src/test/java/edu/craptocraft/props/GasHeaterTest.java
+++ furnace/src/test/java/edu/craptocraft/props/GasHeaterTest.java
@@ -2,28 +2,36 @@ package edu.craptocraft.props;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import edu.craptocraft.furnace.RoomTemperature;
 
 public class GasHeaterTest {
+
+    public static RoomTemperature temperature;
+
+    @BeforeClass
+    public static void createTemperature() {
+        temperature = RoomTemperature.getInstance();
+    }
+
     @Test
     public void testDisangage() {
 
-        RoomTemperature temp = RoomTemperature.getInstance();
-        double initialTemp = temp.getTemperature();
+        double initialTemp = temperature.getTemperature();
         GasHeater gasHeater = new GasHeater();
-        gasHeater.engage(temp);
-        assertEquals(initialTemp + 1, temp.getTemperature(), 0.001);
+        gasHeater.engage(temperature);
+        assertEquals(initialTemp + 1, temperature.getTemperature(), 0.001);
     }
 
     @Test
     public void testEngage() {
-        RoomTemperature temp = RoomTemperature.getInstance();
-        double initialTemp = temp.getTemperature();
+
+        double initialTemp = temperature.getTemperature();
         GasHeater gasHeater = new GasHeater();
-        gasHeater.engage(temp);
-        assertEquals(initialTemp + 1, temp.getTemperature(), 0.001);
+        gasHeater.engage(temperature);
+        assertEquals(initialTemp + 1, temperature.getTemperature(), 0.001);
 
     }
 }

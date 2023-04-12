294907eca7a8eb5bd2ecf6bee3910b751fc07b89
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-04-12 17:33:00 +0200

    feat(Heater): Add DIP in Heater interface

--- furnace/src/main/java/edu/craptocraft/props/GasHeater.java
+++ furnace/src/main/java/edu/craptocraft/props/GasHeater.java
@@ -1,17 +1,18 @@
 package edu.craptocraft.props;
 
 import edu.craptocraft.domain.Heater;
+import edu.craptocraft.furnace.RoomTemperature;
 
 public class GasHeater implements Heater {
 
     @Override
-    public void engage() {
+    public void engage(RoomTemperature temperature) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'engage'");
     }
 
     @Override
-    public void disangage() {
+    public void disangage(RoomTemperature temperature) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'disangage'");
     }
